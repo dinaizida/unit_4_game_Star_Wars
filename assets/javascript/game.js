547af4ab66ec3f390b47b3f4f7858a87f679befe
Dina <>
2018-04-27 13:50:34 -0500

    attack button started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -68,15 +68,28 @@ $("document").ready(function() {
         $("#charactersSection").append(charDiv);
 
     };
-// determine which character selected(when clicked on) and assign its properties to 
-//variables that will be using in the game calculation report in the fight section
-    function onClicFunction (){
+    // determine which character selected(when clicked on) and assign its properties to 
+    //variables that will be using in the game calculation report in the fight section
+    function attackerAssignProperties (){
 
         for (var key in characters){
             if (YourCharacter == characters[key].name){
                 attackerHP = characters[key].healthPoints;
                 attackerAP = characters[key].attackPower;
                 attackerCAP = characters[key].counterAttackPower;
+                characters[key].attack = "true";
+            }
+        }
+    };
+    // determine which character selected(when clicked on) and assign its properties to 
+    //variables that will be using in the game calculation report in the fight section
+    function defenderAssignProperties (){
+
+        for (var key in characters){
+            if (YourDefender == characters[key].name){
+                defenderHP = characters[key].healthPoints;
+                defenderAP = characters[key].attackPower;
+                defenderCAP = characters[key].counterAttackPower;
                 characters[key].attack = "true";
             }
         }
@@ -90,7 +103,9 @@ $("document").ready(function() {
             YourCharacter = myChar.attr("data-name");
             myChar.removeClass("character").addClass("charAttacker");   
         }
-        onClicFunction();
+        // determine which character selected(when clicked on) and assign its properties to 
+        //variables that will be using in the game calculation report in the fight section
+        attackerAssignProperties();
 
         // moves the three remaining characters to " available to attack" section .
         //displays three other characters inside of avalable to attack div
@@ -108,22 +123,58 @@ $("document").ready(function() {
 
         // hide all characters on the top
         $("#charactersSection").hide();
+        $("#CharTitle").hide();
         
 
     });
-//select defender and move him to defender div
+    //select defender and move him to defender div
     $(document).on("click", ".move", function() {
         
         $(this).appendTo("#defender");
         myDef = $(this);
         YourDefender = myDef.attr("data-name");
         myDef.removeClass("move").addClass("charDefender");
-
-        onClicFunction();
+        // determine which character selected(when clicked on) and assign its properties to 
+        //variables that will be using in the game calculation report in the fight section
+        defenderAssignProperties();
     });
 
-   
+        // when the user clicks attack, the player/Your Character's Health Points go down based on the counter attack 
+        // property of the "Defender".Their counter attack decreases your health.
+        $(".attackButton").click(function(){
 
+            // if player clicks attack button and no one is in the "defender" div, then 
+            // game says "no enemy here".
+            // if ($("#defender").children().length == 0) {
+            //     $(".noEnemy").html("No enemy here.");
+            // }
+    
+            if (!(attackerHP < 1) || !(defenderHP < 1)) {
+    
+                // when button is clicked (if both players healthpoints are not 0, 
+                // the game subtracks the defendersCAP from the attackers HP.)
+                attackerHP = (attackerHP - defenderCAP);
+                
+                // writing the attacker/Your Character's new healthpoints to the html. 
+                $("." + YourCharacter).html(attackerHP);
+    
+                // writing the text "You attacked Luke Skywalker for 8 damage".
+                $(".youAttacked").html("You attacked " + YourCharacter + " for " + attackerAP + " damage.");
+    
+                // when button is clicked (if both players healthpoints are not 0, 
+                // the game subtracks the attackers AP points from the defenders HP.)
+                defenderHP = (defenderHP - attackerAP);
+    
+                // writing the text "Luke Skywalker attacked you back for 10 damage."
+                $(".attackedBack").html(YourCharacter + " attacked you back for " + defenderCAP + " damage.");
+            
+                // write the defender's new healthpoints to the html.
+                $("." + YourDefender).html(defenderHP);
+            }
+    
+        }); 
+    
+    
 
 
 });
